e2b3d8947022998e85d889c62057342cc4edc89b
Fermekoo <dandifermeko@gmail.com>
2025-02-23 15:50:59 +0700

    add myname feature

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
 func getHello(w http.ResponseWriter, r *http.Request) {
@@ -11,8 +12,14 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "hello its me")
 }
 
+func myName(w http.ResponseWriter, r *http.Request) {
+	name := strings.TrimPrefix(r.URL.Path, "/name/")
+	io.WriteString(w, fmt.Sprintf("my name is %s", name))
+}
+
 func main() {
 	http.HandleFunc("/hello", getHello)
+	http.HandleFunc("/name/{name}", myName)
 
 	http.ListenAndServe(":3000", nil)
 }
